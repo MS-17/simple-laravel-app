4df9e47fc5fbd0063c300b6462472293f401ce5c
Sergei <106600613+MS-17@users.noreply.github.com>
2023-12-06 14:11:17 +0800

    refactor BookController and add fillable field to all models

--- app/Http/Controllers/BookControllers/BookController.php
+++ app/Http/Controllers/BookControllers/BookController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\BookControllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Str;
+
 
 class BookController extends Controller
 {
-    public function index() {
+    public function index() {        
         return "hello";
     }
 }
